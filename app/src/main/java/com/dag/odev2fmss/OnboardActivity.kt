97f2f17dadf32bc28dcf74fecdfa3b6d07b9d1f3
Bora Bor <bbor98@gmail.com>
2022-09-03 02:49:58 +0300

    Add onboard screen

---
+++ app/src/main/java/com/dag/odev2fmss/OnboardActivity.kt
@@ -0,0 +1,16 @@
+package com.dag.odev2fmss
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.dag.odev2fmss.databinding.ActivityOnboardBinding
+
+class OnboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_onboard)
+    }
+}
