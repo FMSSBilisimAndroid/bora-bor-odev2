555847d6a010678390a39423a056c403f3494373
Bora Bor <bbor98@gmail.com>
2022-09-04 11:51:48 +0300

    Add login functionality

--- app/src/main/java/com/dag/odev2fmss/HomeActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/HomeActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.dag.odev2fmss.databinding.ActivityHomeBinding
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.runBlocking
 
 class HomeActivity : AppCompatActivity() {
 
@@ -16,21 +13,6 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
 
-        binding.user = getUserInfo()
-    }
-
-    /**
-     * Gets user information from the disk.
-     *
-     * @return user information as [User].
-     */
-    private fun getUserInfo(): User = runBlocking {
-        dataStore.data.map { preferences ->
-            val email = preferences[PreferencesKeys.EMAIL] ?: ""
-            val username = preferences[PreferencesKeys.USERNAME] ?: ""
-            val password = preferences[PreferencesKeys.PASSWORD] ?: ""
-
-            User(email = email, username = username, password = password)
-        }.first()
+        binding.user = User.getUserInfo(this)
     }
 }
