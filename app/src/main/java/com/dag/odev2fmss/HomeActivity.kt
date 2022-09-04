b25fa91b987fa5a09ca63ecac8f8c81d8ba00d64
Bora Bor <bbor98@gmail.com>
2022-09-04 09:19:18 +0300

    Add home screen

---
+++ app/src/main/java/com/dag/odev2fmss/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.dag.odev2fmss
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.dag.odev2fmss.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+    }
+}
